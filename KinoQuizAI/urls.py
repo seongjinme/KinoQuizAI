e22144e8a6dabd04f4e67cbe3670b7d7b8379f6f
Seongjin Hong <hongseongjin.to@gmail.com>
2023-03-31 16:58:41 +0900

    Patching for static files

--- KinoQuizAI/urls.py
+++ KinoQuizAI/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 from dotenv import load_dotenv
 import os
@@ -10,4 +12,4 @@ urlpatterns = [
     path('', include('quiz.urls')),
     path(os.environ.get('SECRET_ADMIN_URL') + 'admin/', admin.site.urls),
     path('__reload__', include('django_browser_reload.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
