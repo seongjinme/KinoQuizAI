1b6932f0ff09de537d5ae7b657656b05f0084b92
Seongjin Hong <hongseongjin.to@gmail.com>
2023-04-02 11:18:14 +0900

    Added routes for static files in production env

--- KinoQuizAI/urls.py
+++ KinoQuizAI/urls.py
@@ -2,14 +2,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
-from dotenv import load_dotenv
-import os
 
-
-load_dotenv()
 
 urlpatterns = [
-    path('', include('quiz.urls')),
-    path(os.environ.get('SECRET_ADMIN_URL') + 'admin/', admin.site.urls),
+    path('', include('quiz.urls', namespace='quiz')),
+    path('admin/', admin.site.urls),
     path('__reload__', include('django_browser_reload.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
